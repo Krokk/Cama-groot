6bf3ee0a4d1302b8f200e9cc8875d7069298e164
Krokk <rfabre@student.42.fr>
2017-11-20 15:15:44 +0100

    modifintall

--- install.php
+++ install.php
@@ -52,11 +52,11 @@
 		$conn = new PDO("mysql:host=localhost;dbname=db_camagru", "root", "root");
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$qry = "CREATE TABLE `db_camagru`.`Photos` (
-		`UserId` INT NOT NULL,
-		`photoID` INT NOT NULL,
+		`PhotoID` INT NOT NULL,
+		`username` CHAR NOT NULL,
 		`time` DATETIME NOT NULL,
 		`url` VARCHAR(255) NOT NULL,
-		PRIMARY KEY (`UserId`));
+		PRIMARY KEY (`PhotoID`));
 			";
 		$conn->exec($qry);
 	}
