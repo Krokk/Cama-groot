ba4ee9b3a0342b40e67c291c7d1fbe0ae06546d8
Krokk <rfabre@student.42.fr>
2017-11-22 16:42:31 +0100

    like

--- Like.php
+++ Like.php
@@ -2,32 +2,49 @@
 session_start();
 $_SESSION["message"] = '';
 
+    $picname = $_POST['pic'];
 
-try{
+    try{
     $conn = new PDO("mysql:host=localhost;dbname=db_camagru", "root", "root");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $req = $conn->prepare("SELECT id FROM users where username = :username");
     $req->execute(array(
         ':username' => $_SESSION['LOGGED_ON']
     ));
-    $results = $req->fetch();
-    
-}
-// try{
+    $id = $req->fetch(PDO::FETCH_COLUMN, 0);      
+    }
+    catch(PDOException $e)
+    {
+        echo "Couldn't write in Database: " . $e->getMessage();
+    }
 
-//     $conn = new PDO("mysql:host=localhost;dbname=db_camagru", "root", "root");
-//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//     $req = $conn->prepare('INSERT INTO likes (userid, timet, url) VALUES (:username, NOW() , :url)');
-//     $req->execute(array(
-//         ':username' => $_SESSION['LOGGED_ON'],
-//         ':url' => $filesql
-//     ));
-// }
-catch(PDOException $e)
-{
-    echo "Couldn't write in Database: " . $e->getMessage();
-}
+    try{
+        $conn = new PDO("mysql:host=localhost;dbname=db_camagru", "root", "root");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $req = $conn->prepare("SELECT PhotoID FROM photos where url = :url");
+        $req->execute(array(
+            ':url' => $picname
+        ));
+        $idphoto = $req->fetch(PDO::FETCH_COLUMN, 0);    
+    }
+    catch(PDOException $e)
+    {
+        echo "Couldn't write in Database: " . $e->getMessage();
+    }
 
-var_dump($result);
-// var_dump($_SESSION[LOGGED_ON]);
+    try
+    {
+
+        $conn = new PDO("mysql:host=localhost;dbname=db_camagru", "root", "root");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $req = $conn->prepare('INSERT INTO likes (userid, photoid, timet) VALUES (:userid, :photoid, NOW()');
+        $req->execute(array(
+            ':userid' => $id,
+            ':photoid' => $idphoto,
+        ));
+    }
+    catch(PDOException $e)
+    {
+        echo "Couldn't write in Database: " . $e->getMessage();
+    }
  ?>
